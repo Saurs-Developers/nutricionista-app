470cd947f28d1cc73e2c84bed553a8e5a7970f30
Jefté Medeiros <jefte.medeiros8@gmail.com>
2023-05-25 01:31:38 -0500

    added redirecting to login page component

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
 import { useNavigate } from "react-router-dom"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -8,7 +8,6 @@ import * as z from "zod"
 import { Button } from "@/components/Button"
 import { Input } from "@/components/Input"
 import { Typography } from "@/components/Typography"
-import { useAuth } from "@/hooks/useAuth"
 import { useResetPassword } from "@/hooks/useResetPassword"
 import { resetPasswordSchema } from "@/schemas/resetPassword"
 
@@ -35,10 +34,15 @@ export function ResetPassword() {
 
   useEffect(() => {
     if (isSuccess) {
-      navigate("/login")
+      setIsPasswordReset(true)
       localStorage.removeItem("forgot-password-token")
+      setTimeout(() => {
+        navigate("/login")
+      }, 3000)
     }
   }, [isSuccess, isError])
+
+  const [isPasswordReset, setIsPasswordReset] = useState(false)
 
   return (
     <div className="flex flex-col w-screen h-screen py-8 px-6">
@@ -55,26 +59,34 @@ export function ResetPassword() {
         className="mt-32 flex-1 flex flex-col gap-4 items-center"
       >
         <img className="mb-8" src="logo.png" alt="" />
-        <Typography as="h2" variant="lg" type="body">
-          Preencha os campos abaixo para redefinir sua senha
-        </Typography>
-        <Input
-          label="Senha"
-          {...register("password")}
-          type="password"
-          error={errors.password?.message as string}
-          placeholder="********"
-        />
-        <Input
-          label="Confirmar senha"
-          {...register("password_confirmation")}
-          type="password"
-          error={errors.password_confirmation?.message as string}
-          placeholder="********"
-        />
-        <Button disabled={isLoading} variant="filled">
-          Alterar senha
-        </Button>
+        {isPasswordReset ? (
+          <Typography className="self-start" as="h2" variant="lg" type="body">
+            Senha redefinida com sucesso! Indo para a página de login...
+          </Typography>
+        ) : (
+          <>
+            <Typography as="h2" variant="lg" type="body">
+              Preencha os campos abaixo para redefinir sua senha
+            </Typography>
+            <Input
+              label="Senha"
+              {...register("password")}
+              type="password"
+              error={errors.password?.message as string}
+              placeholder="********"
+            />
+            <Input
+              label="Confirmar senha"
+              {...register("password_confirmation")}
+              type="password"
+              error={errors.password_confirmation?.message as string}
+              placeholder="********"
+            />
+            <Button disabled={isLoading} variant="filled">
+              Alterar senha
+            </Button>
+          </>
+        )}
         {isError && (
           <span className="text-red-500">Falha ao redefinir senha!</span>
         )}
