ac921d127027a61a9536431ab2bfd2016793cdfc
Jefté Medeiros <jefte.medeiros8@gmail.com>
2023-06-15 00:49:26 -0500

    fixed image path

--- src/components/Fallback.tsx
+++ src/components/Fallback.tsx
@@ -1,7 +1,7 @@
 export function Fallback() {
   return (
     <div className="flex items-center justify-center w-screen h-screen">
-      <img className="bounce" src="../../public/logo.png" alt="" />
+      <img className="bounce" src="/logo.png" alt="" />
     </div>
   )
 }
